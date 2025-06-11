e866427cec5de68c87dc87437bf47ae66de14008
Fadhel Khashawi <fkhashawi@gmail.com>
2025-06-11 09:32:19 +0300

    Add capacitor.config.ts with webDir set to dist

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.khashawi.therapod',
+  appName: 'therapod-report-ios',
+  webDir: 'dist'
+};
+
+export default config;
